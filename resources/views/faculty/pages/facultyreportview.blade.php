e69b5adc11f6f6bc1af03984579126953f6bddef
Manoj Ochaney <2016.manoj.ochaney@ves.ac.in>
2019-01-16 02:29:03 +0530

    update report view

--- resources/views/faculty/pages/facultyreportview.blade.php
+++ resources/views/faculty/pages/facultyreportview.blade.php
@@ -3,7 +3,39 @@
 @section('section')
 
 <div class="container-fluid">
-    {{ $eid }}
+    @isset($paper_publications)
+    <p>Paper</p>
+   {{ $paper_publications }}
+   <br><br>
+   @endisset
+   @isset($courses_conducted)
+   <p>CC</p>
+   {{ $courses_conducted }}<br><br>
+   @endisset
+   @isset($courses_attended)
+   <p>CA</p>
+   {{ $courses_attended }}<br><br>
+   @endisset
+   @isset($activities)
+   <p>ACt</p>
+   {{ $activities }}<br><br>
+   @endisset
+   @isset($industry_interactions)
+   <p>II</p>
+   {{ $industry_interactions }}<br><br>
+   @endisset
+   @isset($invitations)
+   <p>In</p>
+   {{ $invitations }}<br><br>
+   @endisset
+   @isset($patents)
+   <p>Pat</p>
+   {{ $patents }}<br><br>
+   @endisset
+   @isset($research_grants)
+   <p>RG</p>
+   {{ $research_grants }}<br><br>
+   @endisset
 </div>
 
 
